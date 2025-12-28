5a5491a38c5a50c20567f571a958166b78e63559
Kashan Adnan <fullstackdevkashan@gmail.com>
2025-12-28 20:55:40 +0500

    added firebase

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-let form = document.getElementById("form")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
 
-function submit(event) {
-    event.preventDefault()
+const firebaseConfig = {
+    apiKey: "AIzaSyBzBmd5piNiNJaZ9orSR_NJVjiRIhZueQ4",
+    authDomain: "saad-aslam-tech.firebaseapp.com",
+    projectId: "saad-aslam-tech",
+    storageBucket: "saad-aslam-tech.firebasestorage.app",
+    messagingSenderId: "341654487346",
+    appId: "1:341654487346:web:e2750ecc996f3b1ee2572b"
+};
 
-    let firstname = document.getElementById("firstname")
-    let lastname = document.getElementById("lastname")
-    let email = document.getElementById("email")
-    let password = document.getElementById("password")
+const app = initializeApp(firebaseConfig);
+window.createYourAccount = () => {
+    alert("Ye chala")
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            // ...
+            alert("Accouny Create Hochuka")
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+            alert("Accouny Create NHi hoa")
 
-    firebase.auth().createUserWithEmailAndPassword(email.value, password.value) 
+        });
 }
-
-form.addEventListener("submit", (e) => submit(e))
